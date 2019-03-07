3b7aa1b1aa22b04a8160c30fb8e335896cb61c8c
hongyan_work <772491158@qq.com>
2019-03-07 23:33:37 +0800

    初始提交

---
+++ src/todo/TodoFooter.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer>
+        <h5>Written by Hongyan</h5>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: "TodoFooter"
+    }
+</script>
+
+<style scoped>
+footer {
+    position: absolute;
+    bottom: 20px;
+    left: 50%;
+    margin-left: -68px;
+}
+footer h5 {
+    text-align: center;
+}
+</style>
---
+++ src/todo/TodoHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <h1>My Todo</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "todo-header"
+    }
+</script>
+
+<style scoped>
+header h1 {
+    text-align: center;
+}
+</style>
